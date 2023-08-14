f7d6b18236b3150fd66f70bf512b22e0e15d5c7b
zjy <1831783781@qq.com>
2023-08-14 18:38:40 +0800

    new

--- config/config.default.js
+++ config/config.default.js
@@ -40,9 +40,9 @@ module.exports = appInfo => {
     client: {
       host: 'localhost',
       port: '3306',
-      user: 'root',
+      user: 'talkbook',
       password: '123456',
-      database: 'account',
+      database: 'talkbook',
     },
     // 是否加载到app上,默认开启
     app: true,
